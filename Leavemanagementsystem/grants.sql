91f057340565756872da3175e28803d1db2a6560
Gokulakannan Rangasamy <gokulakannan.rangasamy@kumaran.com>
2026-04-20 17:11:56 +0530

    chore: remove one-time migration scripts and update .gitignore

--- Leavemanagementsystem/grants.sql
+++
@@ -1,3 +0,0 @@
-GRANT ALL ON SCHEMA public TO leavepaldb;  
-GRANT ALL PRIVILEGES ON DATABASE leavepal TO leavepaldb;  
-ALTER DATABASE leavepal OWNER TO leavepaldb;  
--- migration.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE leave_application ALTER COLUMN duration TYPE NUMERIC(4,1) USING duration::NUMERIC(4,1);
-ALTER TABLE leave_tracker ALTER COLUMN sick_leave_available TYPE NUMERIC(4,1) USING sick_leave_available::NUMERIC(4,1);
-ALTER TABLE leave_tracker ALTER COLUMN casual_leave_available TYPE NUMERIC(4,1) USING casual_leave_available::NUMERIC(4,1);
-ALTER TABLE leave_tracker ALTER COLUMN loss_of_pay_available TYPE NUMERIC(4,1) USING loss_of_pay_available::NUMERIC(4,1);
-ALTER TABLE leave_tracker ALTER COLUMN sick_leave_booked TYPE NUMERIC(4,1) USING sick_leave_booked::NUMERIC(4,1);
-ALTER TABLE leave_tracker ALTER COLUMN casual_leave_booked TYPE NUMERIC(4,1) USING casual_leave_booked::NUMERIC(4,1);
-ALTER TABLE leave_tracker ALTER COLUMN loss_of_pay_booked TYPE NUMERIC(4,1) USING loss_of_pay_booked::NUMERIC(4,1);
